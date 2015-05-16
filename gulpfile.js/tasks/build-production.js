ce4ee5d6aa840774b75cfb19adc8bb11b13bd459
Tyler Schultz <tschultztt@gmail.com>
2015-05-16 18:57:30 -0500

    end process upon build

--- gulpfile.js/tasks/build-production.js
+++ gulpfile.js/tasks/build-production.js
@@ -29,5 +29,6 @@ gulp.task('complete', function() {
   console.log('Project Succsess.'.bold.bgRed);
   console.log('Project Succsess.'.bold.bgWhite);
   console.log('Project Succsess.'.bold.bgBlue);
+  process.exit();
 });
 
