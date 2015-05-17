27bf5458f047f2f780746f249ebaf1b4d4546de4
Tyler Schultz <tschultztt@gmail.com>
2015-05-16 22:15:16 -0500

    change for deploy

--- app/assets/js/pages/magic-show.js
+++ app/assets/js/pages/magic-show.js
@@ -1,15 +1,15 @@
-var exclaimify = require('./../exclaimify');
-// let button = document.getElementById('asyncBtn');
+// var exclaimify = require('./../exclaimify');
+// // let button = document.getElementById('asyncBtn');
 
 
-let alertAsyncMessage = function() {
-  // CommonJS async syntax webpack magic
-  require.ensure([], function(require) {
-    // var message = require("./../components/asyncMessage")
-    console.log('Reading the docs on this.')
-    // alert(exclaimify(message))
-  })
-}
+// let alertAsyncMessage = function() {
+//   // CommonJS async syntax webpack magic
+//   require.ensure([], function(require) {
+//     // var message = require("./../components/asyncMessage")
+//     console.log('Reading the docs on this.')
+//     // alert(exclaimify(message))
+//   })
+// }
 
 
-// button.addEventListener('click', alertAsyncMessage);
+// // button.addEventListener('click', alertAsyncMessage);
