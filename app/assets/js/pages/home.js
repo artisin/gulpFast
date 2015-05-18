1992e831e14d1db84e8ff70ab42d07e53e17707c
Tyler Schultz <tschultztt@gmail.com>
2015-05-17 19:14:15 -0500

    CL of font assets to demo hash

--- app/assets/js/pages/home.js
+++ app/assets/js/pages/home.js
@@ -1,3 +1,6 @@
 console.log('JS: assest also get updates');
-console.log('asset references liks this one:');
+console.log('asset references like this one:');
 console.log('assets/images/gulp.png');
+console.log('Lets Not forget about fonts');
+console.log('assets/fonts/icons.tff');
+console.log('assets/fonts/icons.svg');
