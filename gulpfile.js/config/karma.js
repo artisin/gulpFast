8d915db816f51c085a7d74d85944edc37e0bf15e
Tyler Schultz <tschultztt@gmail.com>
2015-06-23 20:51:10 -0500

    phantomJs added for better test integration

--- gulpfile.js/config/karma.js
+++ gulpfile.js/config/karma.js
@@ -1,17 +1,47 @@
 var config        = require('./'),
-    webpackConfig = require('./webpack')('test'),
-    path          = require('path');
+    webpackConfig = require('./webpack')('test');
 
+//You should prbly config this yourself since it will be dependent on
+//your style and method
 module.exports = {
   frameworks: ['mocha', 'sinon-chai'],
   files: [
-    path.resolve(config.sourceAssets + '/js/__tests__/*')
+    config.sourceAssets + '/js/__tests__/**/*.js',
+  ],
+  exclude:[
+    config.sourceAssets + '/js/__tests__/**/*.txt',
+  ],
+  watch: [
+    //Watch for txt desc to run test
+    config.sourceAssets + '/js/__tests__/**/*.txt',
+    config.sourceAssets + '/js/__tests__/**/*.js',
   ],
   preprocessors: {
-    'app/assets/js/**/__tests__/*': ['webpack']
+    'app/assets/js/__tests__/*': ['webpack', 'sourcemap'],
   },
   webpack: webpackConfig,
+  //Bumped up to comp for buttron complie time
+  browserNoActivityTimeout: 100000,
   singleRun: process.env.TRAVIS_CI === 'true',
   reporters: ['nyan'],
-  browsers: [(process.env.TRAVIS_CI === 'true'? 'Firefox' : 'Chrome')]
-}
+  colors: true,
+  browsers: ['PhantomJS_custom'],
+    // you can define custom flags
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          },
+        },
+        flags: ['--load-images=false'],
+        debug: false
+      }
+    },
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+      exitOnResourceError: true
+    }
+};
