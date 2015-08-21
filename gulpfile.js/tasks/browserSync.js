03c9021780da2c147a00d9ffcd6220788b7eb92a
t-schultz <tschultztt@gmail.com>
2015-08-21 17:40:52 -0500

    deleted, no longer needed

--- gulpfile.js/tasks/browserSync.js
+++
@@ -1,7 +0,0 @@
-var browserSync = require('browser-sync'),
-    gulp        = require('gulp'),
-    config      = require('../config/browserSync');
-
-gulp.task('browserSync', function() {
-  return browserSync(config);
-});
