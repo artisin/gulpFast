b31a8dc98a7bf41f5b96115882c2e0570c5acb73
t-schultz <tschultztt@gmail.com>
2015-08-21 17:41:18 -0500

    migrate to new syntax plus cleaner test runner

--- gulpfile.js/tasks/karma.js
+++ gulpfile.js/tasks/karma.js
@@ -1,22 +1,42 @@
-var gulp     = require('gulp'),
-    karma    = require('karma'),
-    argv     = require('yargs').argv,
-    testOnly = argv._[0] === 'test';
+var gulp  = require('gulp'),
+    Server = require('karma').Server,
+    gulpSequence = require('gulp-sequence'),
+    colors       = require('colors');
 
 var karmaTask = function(done) {
-  var testing = testOnly ? false : true;
-  karma.server.start({
+  new Server({
     configFile: process.cwd() + '/karma.conf.js',
-    //single run when in development, but if 'gulp test' it will be continuous
-    singleRun: testing
+    singleRun: true
   }, function(exitStatus) {
     // Karma's return status is not compatible with gulp's streams
     // See: http://stackoverflow.com/questions/26614738/issue-running-karma-task-from-gulp
     // or: https://github.com/gulpjs/gulp/issues/587 for more information
-    done(exitStatus ? "There are failing unit tests" : undefined);
-  });
+    if (exitStatus) {
+      throw new Error("There are failing unit tests");
+    }else{
+      done();
+    }
+  }).start();
 };
 
-gulp.task('test', karmaTask);
+//start test runne 
+gulp.task('testRunner', karmaTask);
+
+gulp.task('testComplete', function() {
+  console.log('Test Succsess.'.bold.bgRed);
+  console.log('Test Succsess.'.bold.bgWhite);
+  console.log('Test Succsess.'.bold.bgBlue);
+  process.exit();
+});
+
+gulp.task('test', function(cb) {
+  gulpSequence(
+    'testRunner',
+    'testComplete',
+    cb);
+});
 
 module.exports = karmaTask;
+
+
+
