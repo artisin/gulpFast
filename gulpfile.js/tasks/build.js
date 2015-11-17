759f104c8c5c045664402acc6cc2b7cd8f12f629
Tē <te.artisin@gmail.com>
2015-11-17 15:02:09 -0600

    build added

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -2,7 +2,17 @@ var gulp         = require('gulp'),
     gulpSequence = require('gulp-sequence'),
     getEnabledTasks = require('../lib/getEnabledTasks');
 
-gulp.task('build', function(cb) {
+gulp.task('build', ['production-build'], function() {
+  console.log('Project Succsess.'.bold.bgRed);
+  console.log('Project Succsess.'.bold.bgWhite);
+  console.log('Project Succsess.'.bold.bgBlue);
+  if (!deploy) {
+    //Exit gulp
+    process.exit();
+  }
+});
+
+gulp.task('production-build', function(cb) {
   var tasks = getEnabledTasks('production');
   gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'rev', cb);
 });
