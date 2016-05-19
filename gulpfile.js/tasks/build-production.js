5c3f3ccb5d9af0f15611f367d179ed84a95aaa9f
Tē <te.artisin@gmail.com>
2016-05-19 17:46:36 -0500

    shit rebase

---
+++ gulpfile.js/tasks/build-production.js
@@ -0,0 +1,38 @@
+var gulp         = require('gulp'),
+    gulpSequence = require('gulp-sequence'),
+    colors       = require('colors'),
+    del          = require('del'),
+    argv         = require('yargs').argv,
+    deploy       = argv._[0] === 'deploy',
+    config       = require('../config');
+
+
+gulp.task('build', function(cb) {
+  process.env.NODE_ENV = 'production';
+  gulpSequence(
+    'clean', 
+    ['fonts', 'images', 'iconFont', 'svg-sprite'], 
+    ['post-css', 'webpack:production'],
+    'jade', 
+    'cleanTemp', 
+    'complete', 
+    cb);
+});
+
+gulp.task('cleanTemp', function (cb) {
+  del([
+    config.publicTemp,
+  ], cb);
+});
+
+
+gulp.task('complete', function() {
+  console.log('Project Succsess.'.bold.bgRed);
+  console.log('Project Succsess.'.bold.bgWhite);
+  console.log('Project Succsess.'.bold.bgBlue);
+  if (!deploy) {
+    //Exit gulp
+    process.exit();
+  }
+});
+
